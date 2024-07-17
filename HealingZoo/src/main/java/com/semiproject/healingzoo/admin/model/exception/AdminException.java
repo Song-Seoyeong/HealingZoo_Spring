d70afdce40a227c209299b7e4951909a784d68cd
송서영 / 시에나 <168699704+Song-Seoyeong@users.noreply.github.com>
2024-07-17 10:00:29 +0900

    Delete HealingZoo directory

--- HealingZoo/src/main/java/com/semiproject/healingzoo/admin/model/exception/AdminException.java
+++
@@ -1,6 +0,0 @@
-package com.semiproject.healingzoo.admin.model.exception;
-
-public class AdminException extends RuntimeException {
-	public AdminException() {}
-	public AdminException(String msg) {super(msg);}
-}
--- HealingZoo/src/main/java/com/semiproject/healingzoo/board/model/exception/BoardException.java
+++
@@ -1,6 +0,0 @@
-package com.semiproject.healingzoo.board.model.exception;
-
-public class BoardException extends RuntimeException {
-	public BoardException() {}
-	public BoardException(String msg) {super(msg);}
-}
